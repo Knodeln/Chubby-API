b5b324e824e1baf856dda23461d603d187708b90
Knodeln <lukas.hen@hotmail.com>
2023-11-16 11:16:08 +0100

    first commit

---
+++ src/test/java/dev/knodeln/chubby/ChubbyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.knodeln.chubby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChubbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
